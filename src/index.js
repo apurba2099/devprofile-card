98cae2930dfbff656d3d4da3933ee9eea0d42bb0
Apurba Dutta <apurbadutta2099@gmail.com>
2025-04-02 01:53:47 +0530

    Second Project: Component, Props.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+// Profile Pic component
+function Avatar() {
+  return <img className="avatar" src="./assets/apurba.jpg" alt="apurba"></img>;
+}
+
+// Details Component
+function Intro() {
+  const name = "<Apurba Dutta/>";
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>
+        As a student learning React and gaining knowledge in full-stack
+        development, when not coding or working on projects, I enjoy playing
+        mobile📱 games or PC🖥️ games like Call of Duty, cooking and eating.
+      </p>
+    </div>
+  );
+}
+
+// Skill component props
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+//Skillis Component
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML+CSS" emoji="💪" color="#D81159" />
+      <Skill skill="Javascript" emoji="💪" color="#9448BC" />
+      <Skill skill="React" emoji="👶" color="#0496FF" />
+      <Skill skill="Tailwind CSS" emoji="💪" color="#7BF1A8" />
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
